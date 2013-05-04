411c1c871a9a3df65759e8197d7004d4746cc0b4
Fred Trotter <fred.trotter@gmail.com>
2013-05-04 10:08:02 +0000

    now automatically check the file for errors

--- compile.sh
+++ compile.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
+php pdftk_version_check.php
 if [ ! -d "build" ]; then
                 #this is a new git checkout... lets make these directories
-echo "build dir missing... adding them"
+echo "build dir missing... adding them... compiling"
 mkdir build
 mkdir build/arrays
+mkdir tmp
+chown apache:apache tmp
 
 else
 
-echo "build directories found... building"
-rm build/* -f
-rm build/arrays/* -f
+echo "build directories found... compiling"
+rm build/$1.* -f
+rm build/arrays/$1.* -f
 
 fi
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+else
+#this only happens if there is an argument
+
+if [ ! -f pdfsrc/$1.pdf ];
+then
+    echo "I could not find a pdf source file named $1.pdf! You are SOL."
+else
+
 		#This line greates the fdf from the original pdf
 		#eventually this needs to broadened beyond texas but for now the input to this process must be named tx_cred.pdf
-pdftk pdfsrc/tx_cred.pdf generate_fdf output build/tx_cred.fdf
+pdftk pdfsrc/$1.pdf generate_fdf output build/$1.fdf
 		#because of the UTF-16 madness we cannot parse the fdf directly to mine for field names, so we need a map
 		# we use the dump_data_fields_utf8 to make that happen...
-pdftk pdfsrc/tx_cred.pdf dump_data_fields_utf8 output build/tx_cred.map
+pdftk pdfsrc/$1.pdf dump_data_fields_utf8 output build/$1.map
 		# now we need to generate the test form and the test json from the map..
-php mine_field_names.php build/tx_cred.map
+php mine_field_names.php $1 build/$1.map
 		#The next step is to upload the form_fields.json to http://www.jsonschema.net/ and save the result as form_schema.json
 		#That step needs to be done manually... eventually it should be made smarter based on what is actually required in a particular credentialling form...
 		#then we use the same json as the source for our smarty form...
-php smarty_form_generator.php > build/tx_cred.smarty.tpl 
+php smarty_form_generator.php $1 > build/$1.smarty.tpl 
 		#Eventually this should be an alapca form generation instead... in fact, we could go from the json schema to including required rules for alapca... genius... but for later...
 
+php pdf_sanity_check.php build/$1.map
+		#This will generate errors/warnings if something is obviously wrong with 
+		#this whole compile process
+		#This happens when the pdf is not structured properly...
+
+
+fi
+fi
 
 
---
+++ pdf_sanity_check.php
@@ -0,0 +1,146 @@
+<?php
+
+	$map_file_name = $argv[1];
+
+	if(!file_exists($map_file_name)){
+
+		echo "I need a filename of a map to work\n\n";
+		exit();
+	}
+
+	//First we have to parse the contents of the map file..
+	//which we do with lots of explodes...
+
+	$map_contents = file_get_contents($map_file_name);
+
+	$main_field_array = explode("---",$map_contents);
+
+	$main_struct = array();
+	foreach($main_field_array as $field_block){
+
+		$line_array = explode("\n",$field_block);
+		$tmp_array = array();
+		foreach($line_array as $one_line){
+			$array_for_count = explode(': ',$one_line);
+			if(count($array_for_count) ==2){
+				list($key, $value) = explode(': ',$one_line);
+				while(isset($tmp_array[$key])){
+					$key = $key . "+";
+				}
+				$tmp_array[$key] = $value;
+			}
+		}
+	
+
+		//we slowly create an overall data structure for the pdf..
+		if(isset($tmp_array['FieldName'])){
+			$main_struct[$tmp_array['FieldName']] = $tmp_array;
+		}else{
+		//	echo "no field name??\n";
+		//	var_export($tmp_array);
+		}
+	}
+
+
+
+
+	//Second, now that we have the data structure...
+	//lets make sure it follows our compilation rules...
+
+	foreach($main_struct as $field => $field_array){
+
+		$has_error = false;
+
+
+
+		$type = $field_array['FieldType'];
+		
+		if(isset($field_array['FieldNameAlt'])){
+			$desc = $field_array['FieldNameAlt'];
+			if(strlen($desc) > 0){
+				$has_desc = true;
+			}else{
+				$has_desc = false;
+			}
+
+		}else{
+			$has_desc = false;
+
+		}
+
+		if($type == 'Button'){ //radio or checkbox for our purposes
+			//is it a radio or a button..
+			//radios have FieldFlags that are weird..
+
+                        if(isset($field_array['FieldStateOption++'])){
+				$has_error = true;
+                                echo "$field: ERROR. Adobe might be screwing you. There are 3 or more FieldStateOptions for $field normally you only wanti, Off and Yes. Adobe sometimes creates cruft in the fields, so take a look below to see if it is something you did intentionally:";
+			}
+
+
+                        if(strpos($field,'_date') !== false){
+							
+				$has_error = true;
+				echo "$field: Error. this is a button or checkbox, but it has the string _date in the name.. making the compiler confuse it for a date \n";
+
+			}
+
+                        if(strpos($field,'is_') !== false){
+			
+			}else{
+				//this should not happen..
+				$has_error = true;
+				echo "$field: Error. this is a button or checkbox, but it does not have the string 'is_' anywhere \n";
+			}
+
+
+			if($field_array['FieldFlags'] != 0){ //radio buttons seem to have
+						//this set to 49152 
+				//
+				//	RADIO
+				//
+
+				if($has_desc){
+					$has_error = true;//not really but we want to print this field...
+					//then you have found a way to label a radio button!
+					echo "$field: AWESOME! $field is a button, how do you find a way to set FieldNameAlt????\n";
+		
+				}
+			}else{
+				//
+				//	CHECKBOX
+				//
+				if(!$has_desc){
+					$has_error = true;
+					echo "$field: ERROR. $field is a checkbox but it does not have a Tooltip set!!\n"; 
+					
+				}
+			}
+		}
+
+		//
+		//	Rules to enforce for the text types
+		//
+
+		if($type == 'Text'){ //its a text field
+                        if(strpos($field,'is_') !== false){
+				$has_error = true;
+                                //this should not happen.. everything will assume
+				//that it is a logic
+                                echo "$field: Error. this Text field, but it has the string 'is_ in it... making the compiler think it is a checkbox \n";
+
+                        }
+			//see if its a date...
+
+
+		}
+
+		if($has_error){
+               		echo "$field with:\n";
+        	        var_export($field_array);
+	                echo "\n";
+		}
+
+	}
+
+?>
